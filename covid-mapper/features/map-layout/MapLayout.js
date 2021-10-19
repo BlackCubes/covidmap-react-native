8d97ecc488e82613f0fa3a9f6c1295b083a0ed70
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-19 13:36:08 -0700

    feat(maplayout-apis): call the api hooks

--- covid-mapper/features/map-layout/MapLayout.js
+++ covid-mapper/features/map-layout/MapLayout.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useWindowDimensions } from "react-native";
 
 import MapComponent from "../map/Map";
 import Searchbar from "../searchbar/Searchbar";
+import {
+  useGetGlobalCovidStatsQuery,
+  useGetAllCountriesProvincesHistoricalQuery,
+  useGetCountryHistoricalQuery,
+  useGetProvinceHistoricalQuery,
+} from "../../api/covidApi";
 import { OpenSesameButton } from "../../commons/components";
 
-const MapLayout = () => {
+const MapLayout = ({ route }) => {
+  const [searchValue, setSearchValue] = useState("New Zealand");
+
+  // WORLD
+  // - world stats
+  const {
+    data: globalCovidStatsData,
+    isLoading: globalCovidStatsLoading,
+    error: globalCovidStatsError,
+  } = useGetGlobalCovidStatsQuery();
+
+  // - search country/province
+  const {
+    data: allCountriesProvincesHistoricalData,
+    isLoading: allCountriesProvincesHistoricalLoading,
+    error: allCountriesProvincesHistoricalError,
+  } = useGetAllCountriesProvincesHistoricalQuery();
+  const {
+    data: countryHistoricalData,
+    isLoading: countryHistoricalLoading,
+    error: countryHistoricalError,
+  } = useGetCountryHistoricalQuery(searchValue);
+  // const {
+  //   data: provinceHistoricalData,
+  //   isLoading: provinceHistoricalLoading,
+  //   error: provinceHistoricalError,
+  // } = useGetProvinceHistoricalQuery();
+
+  // US
+  // - national stats
+
+  // - search state/county
+
   const { width: mapviewWidth, height: mapviewHeight } = useWindowDimensions();
   const mapviewRegion = {
     latitude: 37.78825,
