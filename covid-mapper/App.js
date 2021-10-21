c1992e667c1cb02377570a9fcc70a00d2c0ad508
Connie Lai <clai011@ucr.edu>
2021-10-21 15:22:59 -0700

    feat(covid-mapper): install expo-location; add useEffect in App.js to access device's current location

--- covid-mapper/App.js
+++ covid-mapper/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import DrawerMenu from './features/drawer/DrawerMenu'
+import DrawerMenu from './features/drawer/DrawerMenu';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let currentLocation = 'Waiting..';
+  if (errorMsg) {
+    currentLocation = errorMsg;
+  } else if (location) {
+    currentLocation = JSON.stringify(location);
+  }
+
+  
   return (
     <Provider store={store}>
-      <DrawerMenu/>
+      <DrawerMenu currentLocation={currentLocation}/>
     </Provider>
   );
 }
