c5e7e15452f466efebb32a163bffbde6bd9db614
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-27 11:53:00 -0700

    feat(searchback-button-styles): add stylings

---
+++ covid-mapper/commons/components/SearchBackButton/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+
+export const SearchBackWrapper = styled.View`
+  width: 70px;
+  height: 25px;
+  padding-right: 5px;
+  padding-left: 5px;
+  background-color: #fbfbfc;
+  border: 0.5px solid #c6c6d1;
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SearchBackTitle = styled.Text`
+  font-size: 12px;
+  color: black;
+`;
