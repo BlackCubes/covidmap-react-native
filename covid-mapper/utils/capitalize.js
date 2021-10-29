2fad9655e46b3422515a873f65bcd3731f142924
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-29 10:15:13 -0700

    docs(capitalize): add documentations

--- covid-mapper/utils/capitalize.js
+++ covid-mapper/utils/capitalize.js
@@ -1,3 +1,11 @@
+/**
+ * Takes in the word and a boolean. Checks to see if the given word is a string, and if not,
+ * then return an empty string. If the second parameter is true, then it capitalizes more
+ * than one word i.e. new york -> New York. Otherwise, it returns the capitalized word.
+ * @param {String} word
+ * @param {Boolean} wantMoreThanOneCapitalWord Default of false.
+ * @returns {String} A string of capitalize word(s).
+ */
 const capitalize = (word, wantMoreThanOneCapitalWord = false) => {
   if (typeof word !== "string") return "";
 
