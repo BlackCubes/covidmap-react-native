199a3d9645c788ba350b05d76dd44a910c6d5a54
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-29 10:08:35 -0700

    feat(capitalize): expand the function to capitalize multiple words

--- covid-mapper/utils/capitalize.js
+++ covid-mapper/utils/capitalize.js
@@ -1,5 +1,13 @@
-const capitalize = (word) => {
+const capitalize = (word, wantMoreThanOneCapitalWord = false) => {
   if (typeof word !== "string") return "";
+
+  if (wantMoreThanOneCapitalWord) {
+    const multiWords = word.split(" ");
+
+    return multiWords
+      .map((wordVal) => wordVal.charAt(0).toUpperCase() + wordVal.slice(1))
+      .join(" ");
+  }
 
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
