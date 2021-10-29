af5aa710091c1672e28710472c28e0268da68e11
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-10-29 09:37:05 -0700

    feat(capitalize-function): create function to capitalize words

---
+++ covid-mapper/utils/capitalize.js
@@ -0,0 +1,7 @@
+const capitalize = (word) => {
+  if (typeof word !== "string") return "";
+
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+export default capitalize;
--- covid-mapper/utils/index.js
+++ covid-mapper/utils/index.js
@@ -1,2 +1,3 @@
+export { default as capitalize } from "./capitalize";
 export { default as cartesianCoordinateConverter } from "./cartesianCoordinateConverter";
 export { default as centroidRegion } from "./centroidRegion";
