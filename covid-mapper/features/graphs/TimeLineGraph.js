14965226ce1ff38a472f909763e08941531f21d5
JayDampitan <jaykennethpd@gmail.com>
2021-10-29 11:11:02 -0700

    feat(graph): adjust the x axis labels vertically to properly display of dates. Change the color of the graph and seperate the cases by using the numSeperator function.

--- covid-mapper/features/graphs/TimeLineGraph.js
+++ covid-mapper/features/graphs/TimeLineGraph.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Dimensions, View, Text } from "react-native";
 import { LineChart } from "react-native-chart-kit";
+import numSeparator from "../../utils/numSeparator";
 
-const chartWidth = Dimensions.get("window").width - 40;
+const chartWidth = Dimensions.get("window").width - 30;
 const chartHeight = Dimensions.get("window").width - 20;
 
 const CasesOverTimeGraph = ({ graphData }) => {
@@ -14,42 +15,45 @@ const CasesOverTimeGraph = ({ graphData }) => {
         width: "100%",
       }}
     >
-      <Text style={{ textAlign: "center" }}>Cases over Time</Text>
+      <Text style={{ textAlign: "center" }}>Last 30 Days cases/time</Text>
       <LineChart
         data={{
           labels: graphData.map((point) => point.x),
           datasets: [
             {
               data: graphData.map((point) => {
-                return point.y;
+                return point.y ;
               }),
             },
           ],
         }}
         width={chartWidth} // from react-native
         height={chartHeight}
-        yAxisInterval={4} // optional, defaults to 1
+        yAxisInterval={1.7} // optional, defaults to 1
         chartConfig={{
           backgroundColor: "#e26a00",
-          backgroundGradientFrom: "blue",
-          backgroundGradientTo: "#000",
-          decimalPlaces: 0, // optional, defaults to 2dp
+          backgroundGradientFrom: "#092979",
+          backgroundGradientTo: "#00b5ff",
+          decimalPlaces: 0,
           color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
           labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
           propsForDots: {
-            r: "2",
+            r: "1",
             strokeWidth: "2",
             stroke: "#ffa726",
           },
         }}
         bezier
         style={{
-          marginVertical: 8,
+          marginVertical: 10,
           borderRadius: 8,
         }}
-        formatXLabel={(value) => parseInt(value.split('/')[1]) % 7 == 0 ? value : ''}
-        // formatYLabel={(value) => value % 2 === 0 ? value : '' }
-        
+        formatXLabel={(value) =>
+          parseInt(value.split("/")[1]) % 2 == 0 ? value : ""
+        }
+        formatYLabel={(value) => numSeparator(value / 1000 ? value : "")}
+        verticalLabelRotation={90}
+        horizontalLabelRotation={-45}
       />
     </View>
   );
