500d8c310ceed62482f06b1ee04709719f6a0819
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-02 16:10:21 -0700

    refactor(drawer-menu): render the world map layout to route

--- covid-mapper/features/drawer/DrawerMenu.js
+++ covid-mapper/features/drawer/DrawerMenu.js
@@ -1,8 +1,16 @@
 import * as React from "react";
-import { Button, View, Text, useWindowDimensions, SafeAreaView } from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  useWindowDimensions,
+  SafeAreaView,
+} from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+
 import DrawerContent from "./DrawerContent";
+import { WorldMapLayout } from "../map-layout";
 import MapLayout from "../map-layout/MapLayout";
 import About from "../about/About";
 import VaccineLayout from "../vaccine-layout/VaccineLayout";
@@ -13,11 +21,14 @@ const Drawer = createDrawerNavigator();
 const DrawerMenu = () => {
   const dimensions = useWindowDimensions();
   let [fontsLoaded] = useFonts({
-    NotoSans_400Regular
+    NotoSans_400Regular,
   });
-  if (!fontsLoaded) return (<SafeAreaView>
-      <Spinner/>
-</SafeAreaView>);
+  if (!fontsLoaded)
+    return (
+      <SafeAreaView>
+        <Spinner />
+      </SafeAreaView>
+    );
   return (
     <NavigationContainer>
       <Drawer.Navigator
@@ -29,17 +40,20 @@ const DrawerMenu = () => {
           },
           drawerType: dimensions.width >= 768 ? "permanent" : "front",
           drawerHideStatusBarOnOpen: true,
-          headerStyle: { 
+          headerStyle: {
             backgroundColor: "#2EC2A0",
             height: 80,
-           },
+          },
           headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold",  fontFamily: 'NotoSans_400Regular' },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontFamily: "NotoSans_400Regular",
+          },
         }}
         drawerContent={(props) => <DrawerContent {...props} />}
       >
         {/* WORLD */}
-        <Drawer.Screen name="World" component={MapLayout} />
+        <Drawer.Screen name="World" component={WorldMapLayout} />
         <Drawer.Screen name="Country Province Stats" component={MapLayout} />
         {/* US */}
         <Drawer.Screen name="US Total" component={MapLayout} />
