6924b986c1badd3c058e8f61c14ed67af69ab0f9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-04 10:04:39 -0700

    feat(vaccine-world-view): create maplayout for the vaccine world

--- covid-mapper/features/map-layout/index.js
+++ covid-mapper/features/map-layout/index.js
@@ -1,4 +1,5 @@
 export { default as USSearchMapLayout } from "./USSearchMapLayout";
 export { default as USViewMapLayout } from "./USViewMapLayout";
+export { default as VaccineWorldViewMapLayout } from "./VaccineWorldViewMapLayout";
 export { default as WorldSearchMapLayout } from "./WorldSearchMapLayout";
 export { default as WorldViewMapLayout } from "./WorldViewMapLayout";
