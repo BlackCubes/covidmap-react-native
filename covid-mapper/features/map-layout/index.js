77cdda3947be2c09ec34f4dcff6c90e6b498a1f9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-04 13:07:56 -0700

    feat(us-vaccine-search): create layout component for vaccine search in the united states

--- covid-mapper/features/map-layout/index.js
+++ covid-mapper/features/map-layout/index.js
@@ -1,5 +1,6 @@
 export { default as USSearchMapLayout } from "./USSearchMapLayout";
 export { default as USViewMapLayout } from "./USViewMapLayout";
+export { default as VaccineUSSearchMapLayout } from "./VaccineUSSearchMapLayout";
 export { default as VaccineUSViewMapLayout } from "./VaccineUSViewMapLayout";
 export { default as VaccineWorldSearchMapLayout } from "./VaccineWorldSearchMapLayout";
 export { default as VaccineWorldViewMapLayout } from "./VaccineWorldViewMapLayout";
