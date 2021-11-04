f0a49633fe3983b9e4fc50ab5c17284e7923c0c8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-04 12:49:44 -0700

    feat(world-vaccine-search): create layout component for vaccine search in the world

--- covid-mapper/features/map-layout/index.js
+++ covid-mapper/features/map-layout/index.js
@@ -1,6 +1,7 @@
 export { default as USSearchMapLayout } from "./USSearchMapLayout";
 export { default as USViewMapLayout } from "./USViewMapLayout";
 export { default as VaccineUSViewMapLayout } from "./VaccineUSViewMapLayout";
+export { default as VaccineWorldSearchMapLayout } from "./VaccineWorldSearchMapLayout";
 export { default as VaccineWorldViewMapLayout } from "./VaccineWorldViewMapLayout";
 export { default as WorldSearchMapLayout } from "./WorldSearchMapLayout";
 export { default as WorldViewMapLayout } from "./WorldViewMapLayout";
