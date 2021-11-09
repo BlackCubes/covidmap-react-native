c4efdb3128a9bb70eec532bb68a4b76d24536a72
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-09 11:53:22 -0800

    refactor(popup-slider-components): create components to refactor the original PopupSlider

---
+++ covid-mapper/features/map-layout/components/popup-slider/PopupSliderObject.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+
+import PopupSliderData from "./PopupSliderData";
+
+const PopupSliderObject = ({ sliderData }) => (
+  <BottomSheetScrollView>
+    <PopupSliderData
+      cases={sliderData.cases}
+      country={sliderData.country}
+      county={sliderData.county}
+      deaths={sliderData.deaths}
+      hasTimelineSequence={sliderData.hasTimelineSequence}
+      population={sliderData.population}
+      provinces={sliderData.provinces}
+      recovered={sliderData.recovered}
+      state={sliderData.state}
+      updatedAt={sliderData.updatedAt}
+    />
+  </BottomSheetScrollView>
+);
+
+export default PopupSliderObject;
--- covid-mapper/features/map-layout/components/popup-slider/index.js
+++ covid-mapper/features/map-layout/components/popup-slider/index.js
@@ -1,2 +1,4 @@
 export { default as PopupSlider } from "./PopupSlider";
+export { default as PopupSliderArray } from "./PopupSliderArray";
 export { default as PopupSliderButton } from "./PopupSliderButton";
+export { default as PopupSliderObject } from "./PopupSliderObject";
