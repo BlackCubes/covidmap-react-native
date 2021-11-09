4e1b124ba71e1ce0755cbec79762e825b3351c6a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-08 16:23:26 -0800

    fix(load-more-function): add if-conditional to the state

--- covid-mapper/utils/loadMore.js
+++ covid-mapper/utils/loadMore.js
@@ -1,6 +1,8 @@
-const loadMore = (data, page, pageSize = 10) => {
+const loadMore = (data, page, pageSize = 10, setPage) => {
   const start = page * pageSize;
-  const end = start + (pageSize - 1);
+  const end = start + pageSize;
+
+  if (start !== data.length - 1) setPage((page += 1));
 
   return data.slice(start, end);
 };
