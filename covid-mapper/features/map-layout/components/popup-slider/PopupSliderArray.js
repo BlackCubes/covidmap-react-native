246dd407598354e02ae807f3dbbfa397b65e0160
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-09 12:20:18 -0800

    fix(reference-error): correct error by importing useState

--- covid-mapper/features/map-layout/components/popup-slider/PopupSliderArray.js
+++ covid-mapper/features/map-layout/components/popup-slider/PopupSliderArray.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BottomSheetFlatList } from "@gorhom/bottom-sheet";
 
 import PopupSliderData from "./PopupSliderData";
