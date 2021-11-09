c5b9e076f35e98b30557f9b0f7a8694c4a9d25f6
Connie Lai <clai011@ucr.edu>
2021-11-09 14:31:03 -0800

    refactor(covid-mapper): improve styling

--- covid-mapper/features/about/About.js
+++ covid-mapper/features/about/About.js
@@ -1,31 +1,39 @@
 import * as React from "react";
-import { SafeAreaView, FlatList, Text, } from "react-native";
+import { SafeAreaView, Text, ScrollView, Image } from "react-native";
 import styled from "styled-components/native";
 import Spinner from "../../commons/components/Spinner/Spinner";
 import { useFonts, NotoSans_400Regular } from "@expo-google-fonts/noto-sans";
 import AboutSection from "./component/AboutSection";
-import {teamData} from '../../utils/teamData';
+import { teamData } from "../../utils/teamData";
 
 const Container = styled.View`
-  height: 100%;
+  height: auto;
   width: 100%;
   display: flex;
   padding-right: 8%;
   padding-left: 8%;
+  padding-top: 4%;
+  margin-bottom: 10%;
   justify-content: center;
 `;
 
 const Title = styled.Text`
   font-weight: bold;
   font-size: 22px;
+  text-decoration: underline;
+  padding: 4% 0% 4% 4%;
 `;
 
-const LineSeparator = styled.View`
-  height: 1px;
-  background-color: #ddd;
-  width: 90%;
+const LogoImage = styled.Image`
   margin: 0 auto;
-  margin: 20px;
+  opacity: 0.8;
+  height: 140px;
+  width: 140px;
+`;
+
+const Intro = styled.Text`
+  text-align: center;
+  font-size: 18px;
 `;
 
 const About = () => {
@@ -33,38 +41,42 @@ const About = () => {
     NotoSans_400Regular,
   });
 
-
   return (
     <SafeAreaView>
       {!fontsLoaded ? (
         <Spinner />
       ) : (
-        <SafeAreaView>
+        <ScrollView>
           <Container>
+            <LogoImage source={require("../../assets/logo.png")} />
+
             <Title
-              style={{ fontFamily: "NotoSans_400Regular", textAlign: "center", padding: '6%' }}
+              style={{
+                fontFamily: "NotoSans_400Regular",
+                textAlign: "center",
+                padding: "6%",
+              }}
             >
               Our Story
             </Title>
-            <Text style={{ fontFamily: "NotoSans_400Regular", textAlign: "center" }}>
-              We are software engineers based in Central California, coming together from various backgrounds to learn React
-              and React Native for app development. 
+            <Intro style={{ fontFamily: "NotoSans_400Regular" }}>
+              We are software engineers based in Central California, coming
+              together from various backgrounds to learn React and React Native
+              for app development.
               {"\n\n"}
-              This project is actually our first mobile app ever, and it is cross-platform (built for both Android and iOS).{"\n\n"} Our team consists of:
-            </Text>
-            <FlatList
-              data={teamData}
-              ItemSeparatorComponent={() => <LineSeparator />}
-              removeClippedSubviews={true}
-              keyExtractor={(item, index) => item + index}
-              renderItem={({ item }) => {
-                  return (
-                    <AboutSection item={item}/>
-                  )
-              }}
-            />
+              This project is actually our first mobile app ever, and it is
+              cross-platform (built for both Android and iOS).{"\n\n"}
+            </Intro>
+            <Title
+              style={{ fontFamily: "NotoSans_400Regular", textAlign: "center" }}
+            >
+              Our Team
+            </Title>
+            {teamData.map((person) => (
+              <AboutSection key={person.name} item={person} />
+            ))}
           </Container>
-        </SafeAreaView>
+        </ScrollView>
       )}
     </SafeAreaView>
   );
--- covid-mapper/features/about/component/AboutSection.js
+++ covid-mapper/features/about/component/AboutSection.js
@@ -1,31 +1,45 @@
 import * as React from "react";
 import { Text } from "react-native";
 import styled from "styled-components/native";
-import {ToadIcon} from '../../../commons/components/Icons'
+import { ToadIcon } from "../../../commons/components/Icons";
 const SectionContainer = styled.View`
-  padding: 6% 0% 10% 0%;
+  margin: 6% 0% 0% 0%;
 `;
 const Name = styled.Text`
   font-weight: bold;
   font-size: 16px;
-  margin-left: 6px;
+  margin-left: 2%;
+  margin-right: 2%;
 `;
 const NameWrapper = styled.View`
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
   justify-content: center;
+  margin: 0 auto;
 `;
 
-const AboutSection =({item})=>{
+const FlipToad = styled.View`
+  transform: scaleX(-1);
+`;
 
-    return (<SectionContainer>
-        <NameWrapper>
-          <ToadIcon/>
-          <Name style={{ fontFamily: "NotoSans_400Regular", marginBottom: '2%',borderColor:'red' }}>{item.name}</Name>
-        </NameWrapper>
-        <Text style={{color: '#3E3E3E'}}>{item.body}</Text>
-      </SectionContainer>)
-}
+const AboutSection = ({ item }) => {
+  return (
+    <SectionContainer>
+      <NameWrapper>
+        <ToadIcon />
+        <Name style={{ fontFamily: "NotoSans_400Regular", borderColor: "red" }}>
+          {item.name}
+        </Name>
+        <FlipToad>
+          <ToadIcon />
+        </FlipToad>
+      </NameWrapper>
+      <Text style={{ color: "#3E3E3E", lineHeight: 20, paddingTop: "4%", fontSize: 16 }}>
+        {item.body}
+      </Text>
+    </SectionContainer>
+  );
+};
 
 export default AboutSection;
