c0c966aa6621ea6bd82a22036e082197eb553b1b
Connie Lai <clai011@ucr.edu>
2021-11-15 13:21:58 -0800

    refactor(VaccineItem): remove unused import

--- covid-mapper/features/vaccine-layout/components/VaccineItem.js
+++ covid-mapper/features/vaccine-layout/components/VaccineItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Text, Pressable, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 import he from "he";
