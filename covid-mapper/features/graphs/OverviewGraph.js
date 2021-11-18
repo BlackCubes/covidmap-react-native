9b9617bb8fd20e47d8dc51addf9397fe0b5d2e36
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-11-18 15:13:13 -0800

    refactor(overview-graph): remove unuse code and comments, and change the layout of the graph

--- covid-mapper/features/graphs/OverviewGraph.js
+++ covid-mapper/features/graphs/OverviewGraph.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Dimensions, View, Text, SafeAreaView } from "react-native";
-import { LineChart, ProgressChart } from "react-native-chart-kit";
-import numSeparator from "../../utils/numSeparator";
+import { ProgressChart } from "react-native-chart-kit";
 import { useFonts, NotoSans_400Regular } from "@expo-google-fonts/noto-sans";
 import Spinner from "../../commons/components/Spinner/Spinner";
-import { Rect, Text as TextSVG, Svg } from "react-native-svg";
 
 const chartWidth = Dimensions.get("window").width - 30;
 const chartHeight = Dimensions.get("window").width - 20;
@@ -14,23 +12,11 @@ const chartConfig = {
   backgroundGradientTo: "#092979",
   backgroundGradientFrom: "#08130D",
   decimalPlaces: 0,
-  color: (opacity = 1) => `rgba(245, 255, 255, ${opacity})`,
+  color: (opacity = 1) => `rgba(148, 234, 220, ${opacity})`,
   labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-  propsForDots: {
-    r: "2", // dot radius
-  },
 };
 
-const OverviewGraph = ({ cases, deaths, recovered }) => {
-  let [tooltipPos, setTooltipPos] = useState({
-    x: 0,
-    y: 0,
-    visible: false,
-    value: 0,
-    toolTipX: "",
-    toolTipY: 0,
-  });
-
+const OverviewGraph = ({ cases, deaths, population, recovered }) => {
   let [fontsLoaded] = useFonts({
     NotoSans_400Regular,
   });
@@ -51,35 +37,28 @@ const OverviewGraph = ({ cases, deaths, recovered }) => {
   const data = {
     labels: [],
     data: [],
-    // legend: [],
   };
 
-  if (cases) {
+  const divideBy = population > 0 ? population : cases;
+
+  const subTitle = population > 0 ? "population" : "cases";
+
+  if (population && population > 0 && cases) {
     data.labels.push("Cases");
 
-    data.data.push({
-      data: cases,
-      color: (opacity = 1) => `rgba(255,167,38,${opacity})`, // yellow
-    });
+    data.data.push(cases / divideBy);
   }
 
   if (deaths) {
     data.labels.push("Deaths");
 
-    data.data.push({
-      data: deaths, //map over deaths
-      color: (opacity = 1) => `rgba(250, 21, 55, ${opacity})`, // red
-    });
+    data.data.push(deaths / divideBy);
   }
 
   if (recovered) {
     data.labels.push("Recovered");
 
-    data.data.push({
-      data: recovered, // if 'recovered' is available
-      color: (opacity = 1) => `rgba(67, 255, 100, ${opacity})`, // green
-    });
-    // data.legend.push("Recovered");
+    data.data.push(recovered / divideBy);
   }
 
   return (
@@ -89,78 +68,17 @@ const OverviewGraph = ({ cases, deaths, recovered }) => {
       }}
     >
       <Text style={{ textAlign: "center", fontFamily: "NotoSans_400Regular" }}>
-        Data from Past 30 Days
+        Summary (on deaths/recovered per {subTitle})
       </Text>
       {/* CHART */}
       <ProgressChart
         data={data}
         width={chartWidth}
         height={chartHeight}
+        strokeWidth={2}
+        radius={19}
         chartConfig={chartConfig}
       />
-
-      {/* <LineChart
-        data={data}
-        width={chartWidth}
-        height={chartHeight}
-        yAxisInterval={1.7} // optional, defaults to 1
-        chartConfig={chartConfig}
-        bezier
-        style={{
-          marginVertical: 10,
-          borderRadius: 8,
-        }}
-        formatXLabel={(value) =>
-          parseInt(value.split("/")[1]) % 2 == 0 ? value : ""
-        }
-        formatYLabel={(value) => numSeparator(value / 1000 ? value : "")}
-        verticalLabelRotation={90}
-        horizontalLabelRotation={-45}
-        decorator={() => {
-          return tooltipPos.visible ? (
-            <View>
-              <Svg>
-                <Rect
-                  x={tooltipPos.x - 25}
-                  y={tooltipPos.y + 18}
-                  width="80"
-                  height="25"
-                  fill="#549185"
-                  rx={6}
-                />
-                <TextSVG
-                  // Text Alignment
-                  x={tooltipPos.x - 14}
-                  y={tooltipPos.y + 32} // increase to lower
-                  fill="white"
-                  fontSize="10"
-                  style={{ textAlign: "center" }}
-                >
-                  {numSeparator(tooltipPos.value)}
-                </TextSVG>
-              </Svg>
-            </View>
-          ) : null;
-        }}
-        onDataPointClick={(data) => {
-          let isSamePoint = tooltipPos.x === data.x && tooltipPos.y === data.y; // boolean
-
-          isSamePoint
-            ? setTooltipPos((previousState) => {
-                return {
-                  ...previousState,
-                  value: data.value,
-                  visible: !previousState.visible,
-                };
-              })
-            : setTooltipPos({
-                x: data.x,
-                value: data.value,
-                y: data.y,
-                visible: true,
-              });
-        }}
-      /> */}
     </View>
   );
 };
