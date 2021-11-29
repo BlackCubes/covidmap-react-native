110037bcc2f17429e9ab6d3ca1817468cdc5404a
JayDampitan <jaykennethpd@gmail.com>
2021-11-29 14:03:16 -0800

    style(previousRegionButton): add margin left to previous region button to place it in the center of the screen.

--- covid-mapper/commons/components/PreviousRegionButton/styles.js
+++ covid-mapper/commons/components/PreviousRegionButton/styles.js
@@ -2,13 +2,13 @@ import styled from "styled-components/native";
 
 export const PreviousRegionWrapper = styled.View`
   height: 25px;
-  padding-right: 10px;
-  padding-left: 10px;
+  padding: 0 10px;
   background-color: #808B89;
   border: 0.5px solid #c6c6d1;
   border-radius: 50px;
   align-items: center;
   justify-content: center;
+  margin-left: 110px;
 `;
 
 export const PreviousRegionTitle = styled.Text`
